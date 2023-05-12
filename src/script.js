fd22751f9c29a00611ebfaac01f52d3d17881393
mimansha-swarup <mimanshaswarup@gmail.com>
2023-05-12 18:08:15 +0530

    11- textures

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "lil-gui";
-import gsap from 'gsap'
+import gsap from "gsap";
 
 const canvas = document.querySelector("canvas.webgl");
 
@@ -10,24 +10,55 @@ const canvas = document.querySelector("canvas.webgl");
 
 const gui = new dat.GUI();
 const parameter = {
-  spin:()=>{
-    gsap.to(mesh.rotation,{duration:3, y:mesh.rotation.y + Math.PI*2})
-  }
-}
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 3, y: mesh.rotation.y + Math.PI * 2 });
+  },
+};
 
-window.addEventListener("keydown",(event)=>{
-  if(event.key==="h"){
-    if(gui._hidden){
-      gui.show()
-    }else gui.hide()
+window.addEventListener("keydown", (event) => {
+  if (event.key === "h") {
+    if (gui._hidden) {
+      gui.show();
+    } else gui.hide();
   }
-})
+});
 // scene
 const scene = new THREE.Scene();
 
+// THEM<E
+//  What happens in BG
+// const image  = new Image()
+// const texture  = new THREE.Texture(image)
+
+// image.onload = ()=>{
+//   texture.needsUpdate= true
+
+// }
+// image.src="/textures/door/color.jpg"
+const loadingManager = new THREE.LoadingManager();
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load("/textures/door/color.jpg");
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+const ambientOcclusionTexture = textureLoader.load("/textures/door/ambientOcclusion.jpg");
+
+// colorTexture.repeat.x=2
+// colorTexture.repeat.y=3
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+
+// colorTexture.offset.x =0.5
+// colorTexture.offset.y =0.5
+
+colorTexture.rotation =Math.PI/4
+colorTexture.center.x =0.5
+colorTexture.center.y =.5
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
@@ -39,7 +70,7 @@ gui.add(mesh.position, "y").min(-2).max(2).step(0.01).name("elevation");
 
 gui.add(mesh, "visible").name("Hide");
 gui.add(material, "wireframe").name("Show Wireframe");
-gui.addColor(material, 'color')
+gui.addColor(material, "color");
 // We can chain On Change here
 gui.add(parameter, "spin").name("Spin");
 
