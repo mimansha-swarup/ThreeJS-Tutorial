f5aa47b0fc57ac70301466a8f375c6e2a688b9e7
mimansha-swarup <mimanshaswarup@gmail.com>
2023-05-30 21:05:14 +0530

    donut number educe

--- src/script.js
+++ src/script.js
@@ -58,7 +58,7 @@ fontLoader.load("fonts/helvetiker_regular.typeface.json", (font) => {
   scene.add(text);
 
   const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-  for (let i = 0; i < 300; i++) {
+  for (let i = 0; i < 100; i++) {
     const donut  = new THREE.Mesh(donutGeometry, material)
     donut.position.x= (Math.random()-0.5)*10
     donut.position.y= (Math.random()-0.5)*10
